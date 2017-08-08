695f9849e40a2f56a122cfc98f3ad1e3758ee476
amonkonanBYF <fabianbrin@gmail.com>
2017-08-08 18:06:59 +0200

    fabien
    Cubecode

---
+++ cubeclone/src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use AppBundle\Entity\Mission;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+
+    $missions = new Mission();
+
+    $em = $this->container->get('doctrine')->getManager();
+    $missions = $em->getRepository(Mission::class)->findAll();
+
+
+    return $this->render('default/index.html.twig',compact('missions'));
+    }
+    /**
+  * @Route("/{id}", name="mission_path", requirements={"id" : "[0-9]+"})
+  * @Method("GET")
+  */
+ public function showAction(Mission $mission){
+
+    return $this->render('missions/show.html.twig',compact('mission'));
+
+ }
+
+
+
+}
